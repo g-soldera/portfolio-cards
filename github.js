7ec291dacabfd4ce771e131990264dfd049693c5
g-soldera <gustavosevere@gmail.com>
2023-06-06 12:41:17 -0300

    octokit on cdn

--- github.js
+++ github.js
@@ -1,5 +1,5 @@
 // consumir o octokit
-import { Octokit } from "@@octokit"
+import { Octokit } from "https://cdn.skypack.dev/octokit"
 
 window.Octokit = new Octokit({
   auth: "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",
