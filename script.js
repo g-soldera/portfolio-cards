6da85ba39f8c73f1c8b67283b334d3c811a63b52
g-soldera <gustavosevere@gmail.com>
2023-06-07 10:01:30 -0300

    mobile version

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function checkColorScheme() {
   }
 }
 window.addEventListener("load", checkColorScheme)
+
+var menuToggle = document.querySelector(".menu-toggle")
+var overlay = document.querySelector(".overlay")
+var sideBlock = document.querySelector(".sideblock")
+
+menuToggle.addEventListener("click", function () {
+  sideBlock.classList.toggle("open")
+  menuToggle.classList.toggle("hide")
+  overlay.classList.toggle("show")
+})
+
+document.addEventListener("mouseup", function (e) {
+  var container = sideBlock
+
+  // Se o clique for fora do side block, feche-o
+  if (!container.contains(e.target) && container.classList.contains("open")) {
+    container.classList.remove("open")
+    menuToggle.classList.remove("hide")
+    overlay.classList.remove("show")
+  }
+})
